89b6fd3804f77ae7a076d1c4bdc5232d1e544e86
Andy Chen <chenyangpoppin@hotmail.com>
2023-06-22 18:11:34 +0800

    feat: helloworld

---
+++ Ch2-Basics/helloworld.py
@@ -0,0 +1,11 @@
+#
+# hello world and main function
+#
+
+def main():
+    print("Hello Python!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
